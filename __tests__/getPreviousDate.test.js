b4178b32f51b26bfa23d1faf7a0bcf288133b971
adarshkarne <55990363+adarshkarne@users.noreply.github.com>
2024-04-25 21:00:21 +0000

    initial commit

--- __tests__/getPreviousDate.test.js
+++ __tests__/getPreviousDate.test.js
@@ -3,5 +3,8 @@ const getNextDate = require('../modules/getPreviousDate');
 
 test('Previous date test', () => {
     // expect(getPreviousDate(4, 16, 2024)) ....
+    expect(getNextDate(4, 15, 2024)).toEqual({ month: 4, day: 14, year: 2024 });
+    expect(getNextDate(3, 1, 2023)).toEqual({ month: 2, day: 28, year: 2023 });
+    expect(getNextDate(1, 1, 2022)).toEqual({ month: 12, day: 31, year: 2021 });
     
 });
